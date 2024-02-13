cc801608029c6d9ba5aa7bf6e5fd44cbfebe3c95
utkirbek <utkirbektoirov1@gmail.com>
2024-02-13 12:13:03 +0500

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -96,8 +96,8 @@ watchEffect(() => {
   tg.MainButton.setParams({
     text: "Tayyor",
   });
-  tg.expand();
-  tg.ready();
+  //   tg.expand();
+  //   tg.ready();
   tg.onEvent("mainButtonClicked", onSendData);
 });
 </script>
