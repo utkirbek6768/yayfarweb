1ffab24f7fb8f7fbc55c6ea3893f436bbe3af5a5
utkirbek <utkirbektoirov1@gmail.com>
2024-04-29 11:19:16 +0500

    commit

--- src/views/CreatePostView.vue
+++ src/views/CreatePostView.vue
@@ -10,6 +10,7 @@ import axios from "axios";
 import { ref } from "vue";
 
 const file = ref(null);
+const BOT_TOKEN = "6302856184:AAFr7Wan3KQJlg0d3DLiCZZ6keAuT6zZU98";
 
 const handleFileUpload = (event) => {
   file.value = event.target.files[0];
@@ -25,13 +26,10 @@ const sendPicture = async () => {
   formData.append("photo", file.value);
 
   try {
-    await axios.post(
-      `https://api.telegram.org/bot${YOUR_BOT_TOKEN}/sendPhoto`,
-      {
-        chat_id: YOUR_CHAT_ID,
-        photo: formData,
-      }
-    );
+    await axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto`, {
+      chat_id: 177482674,
+      photo: formData,
+    });
     console.log("Picture sent successfully");
   } catch (error) {
     console.error("Error sending picture:", error);
